98fe5a4db913b6f1bc15e0b20c59cc21e75e8fa3
rezarria <votanamcb@outlook.com>
2023-11-07 05:46:21 +0700

    up

--- src/app/admin/role/List.tsx
+++ src/app/admin/role/List.tsx
@@ -1,18 +1,35 @@
 "use client";
 
+import { connect } from "@/lib/Axios";
 import { Table } from "antd";
-import { useState } from "react";
+import { AnyObject } from "antd/es/_util/type";
+import { ColumnsType } from "antd/es/table";
+import { HttpStatusCode } from "axios";
+import { useCallback, useEffect, useState } from "react";
 
-export default function List<T>(
+export default function List<T extends AnyObject>(
   props: Readonly<{
     url: string;
+    columnsDef: ColumnsType<T>;
   }>,
 ) {
   const [data, setData] = useState<T[]>([]);
-
+  const fetch = useCallback(
+    (url: string) => {
+      connect.get<T[]>(url).then((res) => {
+        if (res.status == HttpStatusCode.Ok) {
+          setData([...data, ...res.data]);
+        }
+      });
+    },
+    [data],
+  );
+  useEffect(() => {
+    fetch(props.url);
+  }, [fetch, props.url]);
   return (
     <>
-      <Table />
+      <Table columns={props.columnsDef} />
     </>
   );
 }
