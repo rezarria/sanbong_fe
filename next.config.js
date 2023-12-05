0184db004978a74dbf0210441257ea2ebdf97db2
rezarria <votanamcb@outlook.com>
2023-12-06 03:33:55 +0700

    đặt kiểu svg và thêm icon sân bóng

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// @ts-check
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -11,6 +12,25 @@ const nextConfig = {
       },
     ],
   },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(rule=> rule.test?.test?.('.svg'),)
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: {
+          not: [...fileLoaderRule.resourceQuery.not, /url/]
+        },
+        use: ['@svgr/webpack']
+      }
+    )
+    return config
+  }
 }
 
 module.exports = nextConfig
